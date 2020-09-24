99ea56dc47377c843c2868e5ec2563fcfadbeb77
sahilgargofficial <sahilgargofficial@gmail.com>
2020-09-24 21:45:01 +0530

    fix 2

--- passportSetting.js
+++ passportSetting.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const FaceBookStrategy = require('passport-facebook')
+const FaceBookStrategy = require('passport-facebook').Strategy
 const dotenv = require('dotenv')
 dotenv.config()
 
